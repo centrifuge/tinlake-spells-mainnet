b0b90fe618f9343bbf4e74ec5a25fef39c174a7b
ilinzweilin <alina@centrifuge.io>
2020-12-18 17:55:19 +0100

    add spell for ns2

--- src/spell.t.sol
+++ src/spell.t.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.5.15 <0.6.0;
 import "ds-test/test.sol";
 import "./../src/spell.sol";
 
-
 interface AuthLike {
     function wards(address) external returns(uint);
     function rely(address) external;
@@ -36,9 +35,11 @@ contract TinlakeSpellsTest is DSTest {
     function testCast() public {
         // tinlake contracts 
         address seniorMemberList_ = address(spell.SENIOR_MEMBERLIST());
-        address juniorMemberList_ = address(spell.SENIOR_MEMBERLIST());
-        //address assessorAdmin_ = address(spell.ASSESSOR_ADMIN());
-
+        address juniorMemberList_ = address(spell.JUNIOR_MEMBERLIST());
+        address assessorAdmin_ = address(spell.ASSESSOR_ADMIN());
+        address navFeed_ = spell.NAV_FEED();    
+        NAVFeedLike navFeed = NAVFeedLike(navFeed_);
+        
         // addresses for permissions setup
         address seniorMemberListAdmin1_ = address(spell.SENIOR_MEMBERLIST_ADMIN1());
         address seniorMemberListAdmin2_ = address(spell.SENIOR_MEMBERLIST_ADMIN2());
@@ -47,20 +48,21 @@ contract TinlakeSpellsTest is DSTest {
         address juniorMemberListAdmin1_ = spell.JUNIOR_MEMBERLIST_ADMIN1();
         address juniorMemberListAdmin2_ = spell.JUNIOR_MEMBERLIST_ADMIN2();
         address juniorMemberListAdmin3_ = spell.JUNIOR_MEMBERLIST_ADMIN3();
-        AuthLike a = AuthLike(address(seniorMemberList_));
-        a.wards(address(seniorMemberListAdmin1_));
-        emit log_named_address("moin", seniorMemberListAdmin1_);
-        emit log_named_address("moin", seniorMemberList_);
+
+        address assessorAdminAdmin1_ = spell.ASSESSOR_ADMIN_ADMIN1();
+        address assessorAdminAdmin2_ = spell.ASSESSOR_ADMIN_ADMIN2();
+       
         // make sure permissions are not set yet
         assertHasNoPermissions(seniorMemberList_, seniorMemberListAdmin1_);
-         emit log_named_uint("moin", 1);
         assertHasNoPermissions(seniorMemberList_, seniorMemberListAdmin2_);
-         emit log_named_uint("moin", 1);
         assertHasPermissions(seniorMemberList_, seniorMemberListAdminRemove_);
-        emit log_named_uint("moin", 1);
+
         assertHasNoPermissions(juniorMemberList_, juniorMemberListAdmin1_);
         assertHasNoPermissions(juniorMemberList_, juniorMemberListAdmin2_);
         assertHasNoPermissions(juniorMemberList_, juniorMemberListAdmin3_);
+
+        assertHasNoPermissions(assessorAdmin_, assessorAdminAdmin1_);
+        assertHasNoPermissions(assessorAdmin_, assessorAdminAdmin2_);
 
         // give spell permissions on root contract
         AuthLike(root_).rely(spell_);
@@ -75,6 +77,11 @@ contract TinlakeSpellsTest is DSTest {
         assertHasPermissions(juniorMemberList_, juniorMemberListAdmin2_);
         assertHasPermissions(juniorMemberList_, juniorMemberListAdmin3_);
 
+        assertHasPermissions(assessorAdmin_, assessorAdminAdmin1_);
+        assertHasPermissions(assessorAdmin_, assessorAdminAdmin2_);
+
+         // check discountRate set
+        assertEq(navFeed.discountRate(), spell.discountRate());
     }
 
     function testFailCastNoPermissions() public {        
@@ -95,11 +102,7 @@ contract TinlakeSpellsTest is DSTest {
     }
 
     function assertHasNoPermissions(address con, address ward) public {
-        emit log_named_uint("moin", 1);
-        emit log_named_address("con", con);
-        emit log_named_address("ward", ward);
         uint perm = AuthLike(con).wards(ward);
-        emit log_named_uint("moin", perm);
         assertEq(perm, 0);
     }
 }
