cda8c35bf78a84e283f10a817c6b8bcbb81c66cb
Jeroen Offerijns <jeroen@offerijns.com>
2021-02-09 15:44:46 +0100

    feat: set up memberadmin spell

---
+++ src/spell.sol
@@ -0,0 +1,79 @@
+// Copyright (C) 2020 Centrifuge
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.5.15 <0.6.0;
+
+contract MemberlistLike {
+    function rely(address) external;
+}
+
+contract MemberAdminLike {
+    function relyAdmin(address) external;
+}
+
+// This spell adds wards to all memberlists
+contract TinlakeSpell {
+
+    bool public done;
+    string constant public description = "Tinlake Mainnet Spell";
+
+    // MAINNET ADDRESSES
+    // Member admin contract
+    address constant public MEMBERADMIN = 0xB7e70B77f6386Ffa5F55DDCb53D87A0Fb5a2f53b;
+   
+    // Active memberlists
+    address constant public SENIOR_MEMBERLIST_BL1 = 0x9b0b65ad4b477F38864BE014abd40C97c385a2dd;
+    address constant public JUNIOR_MEMBERLIST_BL1 = 0x000620c6ef2996b2107d91707830751AcaFF90Bb;
+    address constant public SENIOR_MEMBERLIST_CF4 = 0x26129802A858F3C28553f793E1008b8338e6aEd2;
+    address constant public JUNIOR_MEMBERLIST_CF4 = 0x4CA09F24f3342327da42d2b6035af741fC1AeB4A;
+    address constant public SENIOR_MEMBERLIST_DBF1 = 0x58e211AFED5843813dD09F185A0eb0bdb6BbCa72;
+    address constant public JUNIOR_MEMBERLIST_DBF1 = 0xfaEea82d9Db2737B959e3BeF985A91c90abC9486;
+    address constant public SENIOR_MEMBERLIST_FF1 = 0x6e79770F8B57cAd29D29b1884563556B31E792b0;
+    address constant public JUNIOR_MEMBERLIST_FF1 = 0x3FBD11B1f91765B32BD1231922F1E32f6bdfCB1c;
+    address constant public SENIOR_MEMBERLIST_HTC2 = 0x1Bc55bcAf89f514CE5a8336bEC7429a99e804910;
+    address constant public JUNIOR_MEMBERLIST_HTC2 = 0x0b635CD35fC3AF8eA29f84155FA03dC9AD0Bab27;
+    address constant public SENIOR_MEMBERLIST_NS2 = 0x5B5CFD6E45F1407ABCb4BFD9947aBea1EA6649dA;
+    address constant public JUNIOR_MEMBERLIST_NS2 = 0x42C2483EEE8c1Fe46C398Ac296C59674F9eb88CD;
+    address constant public SENIOR_MEMBERLIST_PC3 = 0x1770129cd23C680C1c52C8D3a9c3D527B73CE1de;
+    address constant public JUNIOR_MEMBERLIST_PC3 = 0xA768ACDe6B95720ba926E4a615ACA733e51F6FD1;
+   
+    // Onboard API admin account
+    address constant public ONBOARD_API = 0x0b4455B58d308A66d789cFB7B5D9A62E6D51490e;   
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        execute();
+    }
+
+    function execute() internal {
+        // Make ward on all memberlists
+        MemberlistLike(address(SENIOR_MEMBERLIST_BL1)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_BL1)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_CF4)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_CF4)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_DBF1)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_DBF1)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_FF1)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_FF1)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_HTC2)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_HTC2)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_NS2)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_NS2)).rely(MEMBERADMIN);
+        MemberlistLike(address(SENIOR_MEMBERLIST_PC3)).rely(MEMBERADMIN);
+        MemberlistLike(address(JUNIOR_MEMBERLIST_PC3)).rely(MEMBERADMIN);
+
+        // TODO: Make onboard API an admin on the member admin
+        MemberAdminLike(address(MEMBERADMIN)).relyAdmin(ONBOARD_API);
+    }   
+}
