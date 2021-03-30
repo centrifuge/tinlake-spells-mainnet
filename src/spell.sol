be90ca62add68a11c99df0371914df7701896119
Jeroen Offerijns <jeroen@offerijns.com>
2021-03-30 09:33:01 +0200

    feat: add spell for pz1

--- src/spell.sol
+++ src/spell.sol
@@ -13,52 +13,27 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 pragma solidity >=0.5.15 <0.6.0;
 
-interface TinlakeRootLike {
+interface ShelfLike {
+    function depend(bytes32 contractName, address addr) external;
+}
+
+interface RootLike {
     function relyContract(address, address) external;
-    function denyContract(address, address) external;
+    function deploy() external;
 }
 
-interface CoordinatorLike {
-    function file(bytes32 name, uint value) external;
-    function minimumEpochTime() external returns(uint);
-}
-
-interface NAVFeedLike {
-    function file(bytes32 name, uint value) external;
-    function file(bytes32 name, uint risk_, uint thresholdRatio_, uint ceilingRatio_, uint rate_, uint recoveryRatePD_) external;
-    function discountRate() external returns(uint);
-}
-
-// This spell fixes the permissions and risk groups of the FC1 pool
+// This spell fixes the permissions and risk groups of the PZ pool
 contract TinlakeSpell {
 
     bool public done;
     string constant public description = "Tinlake Mainnet Spell";
 
-    // FC1 root contracts
-    address constant public ROOT = 0x714D520CfAC2027834c8Af8ffc901855c3aD41Ec;
-    address constant public SENIOR_MEMBERLIST = 0xD0f425753437176923477F5b310bbd1a2e0A4253;
-    address constant public JUNIOR_MEMBERLIST = 0x0F8129ce11653ED7fb83B60d8580b86022Bf0aeF;
-    address constant public ASSESSOR = 0xab321CDc90b8882D19c2866b377b450284501219;
-    address constant public ASSESSOR_ADMIN = 0x1C422e1cf17CCDBb78A795EfeA637d569C830c1d;
-    address constant public FEED = 0x30E3f738f22f5a4671d1252793deB6e657e4b8AA;
-    address constant public COORDINATOR = 0x44E9868B37e93F560Deccd5d8d62b26bC62Dce2d;
-    address constant public PILE = 0x99D0333f97432fdEfA25B7634520d505e58B131B;
-   
-    // permissions to be set
-    address constant public ADMIN_TO_BE_REMOVED = 0x0A735602a357802f553113F5831FE2fbf2F0E2e0;
-    address constant public NEW_ADMIN1 = 0xcC7f213f835875175ec150c07AC774D57200f4d8;
-    address constant public NEW_ADMIN2 = 0x71d9f8CFdcCEF71B59DD81AB387e523E2834F2b8;
-    address constant public NEW_ADMIN3 = 0xa7Aa917b502d86CD5A23FFbD9Ee32E013015e069;
-    address constant public NEW_ADMIN4 = 0xfEADaD6b75e6C899132587b7Cb3FEd60c8554821;
-    address constant public NEW_ADMIN5 = 0xC3997Ef807A24af6Ca5Cb1d22c2fD87C6c3b79E8;
-    address constant public NEW_ADMIN6 = 0xd60f7CFC1E051d77031aC21D9DB2F66fE54AE312;
-    address constant public ORACLE = 0xCbd552356e4C2865387a4470D872265cD0FEFB34;
-
-    // new minEpochTime
-    uint constant public minEpochTime = 1 days - 10 minutes;
-
-    uint256 constant ONE = 10**27;
+    // PZ1 contracts
+    address constant public ROOT = 0x92332a9831AC04275bC0f22b9140b21c72984EB8;
+    address constant public SHELF = 0xBd9F2C1B2983e7923b3029c11BA485496F938564;
+    address DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address wCUSD = 0xad3E3Fc59dff318BecEaAb7D00EB4F68b1EcF195;
+    address RESERVE = 0x7f5dEa6c463A7250c53F1347f82B506F40E1b0cB;
 
     function cast() public {
         require(!done, "spell-already-cast");
@@ -67,51 +42,9 @@ contract TinlakeSpell {
     }
 
     function execute() internal {
-      TinlakeRootLike root = TinlakeRootLike(address(ROOT));
-      CoordinatorLike coordinator = CoordinatorLike(address(COORDINATOR));
-      NAVFeedLike navFeed = NAVFeedLike(address(FEED));
-
-      // Setup
-      root.relyContract(COORDINATOR, address(this)); // required to modify min epoch time
-      root.relyContract(FEED, address(this)); // required to file riskGroups
-
-      // Fix config
-      coordinator.file("minimumEpochTime", minEpochTime);
-
-      // File risk groups
-      // risk group: 5, APR: 12.000%%
-      navFeed.file("riskGroup", 5, ONE, 75 * 10 ** 25, uint(1000000003805175038051750380), 99.9589*10**25);
-
-      // Remove incorrect permissions
-      root.denyContract(SENIOR_MEMBERLIST, ADMIN_TO_BE_REMOVED);
-      root.denyContract(JUNIOR_MEMBERLIST, ADMIN_TO_BE_REMOVED);
-      root.denyContract(ASSESSOR, ADMIN_TO_BE_REMOVED);
-      root.denyContract(ASSESSOR_ADMIN, ADMIN_TO_BE_REMOVED);
-      root.denyContract(FEED, ADMIN_TO_BE_REMOVED);
-      root.denyContract(COORDINATOR, ADMIN_TO_BE_REMOVED);
-    
-      // Add correct permissions
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN1);
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN2);
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN3);
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN4);
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN5);
-      root.relyContract(ASSESSOR_ADMIN, NEW_ADMIN6);
-
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN1);
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN2);
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN3);
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN4);
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN5);
-      root.relyContract(JUNIOR_MEMBERLIST, NEW_ADMIN6);
-
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN1);
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN2);
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN3);
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN4);
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN5);
-      root.relyContract(SENIOR_MEMBERLIST, NEW_ADMIN6);
-
-      root.relyContract(FEED, ORACLE);
+        ShelfLike(SHELF).depend("token", DAI);
+        ShelfLike(SHELF).depend("lender", ROOT);
+        ShelfLike(SHELF).depend("token", wCUSD);
+        ShelfLike(SHELF).depend("lender", RESERVE);
     }   
 }
--- src/spell.t.sol
+++ src/spell.t.sol
@@ -14,10 +14,6 @@ contract Hevm {
     function store(address, bytes32, bytes32) public;
 }
 
-interface PileLike {
-    function rates(uint rate) external view returns (uint, uint, uint ,uint48, uint);
-}
-
 contract TinlakeSpellsTest is DSTest {
 
     Hevm public hevm;
@@ -25,7 +21,6 @@ contract TinlakeSpellsTest is DSTest {
     
     address root_;
     address spell_;
-    address constant public PILE = 0x99D0333f97432fdEfA25B7634520d505e58B131B;
 
     function setUp() public {
         spell = new TinlakeSpell();
@@ -35,69 +30,16 @@ contract TinlakeSpellsTest is DSTest {
         
         // cheat: give testContract permissions on root contract by overriding storage 
         // storage slot for permissions => keccak256(key, mapslot) (mapslot = 0)
-        hevm.store(root_, keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+        hevm.store(spell.SHELF(), keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
     }
 
-    function testCast() public {
-        address assessor_ = spell.ASSESSOR();
-        address assessorAdmin_ = spell.ASSESSOR_ADMIN();
-        address coordinator_ = spell.COORDINATOR();
-        address navFeed_ = spell.FEED();
-        address juniorMemberlist_ = spell.JUNIOR_MEMBERLIST();
-        address seniorMemberlist = spell.SENIOR_MEMBERLIST();
-        address adminToBeRemoved_ = spell.ADMIN_TO_BE_REMOVED();
-        address newAdmin_ = spell.NEW_ADMIN1();
+    function testFailDeploy() public {
+        RootLike(root_).deploy();
+    }
 
-        CoordinatorLike coordinator = CoordinatorLike(coordinator_);
-        NAVFeedLike navFeed = NAVFeedLike(navFeed_);
-        PileLike pile = PileLike(PILE);
-
-        // check if admin to be removed has permissions
-        assertHasPermissions(assessor_, adminToBeRemoved_);
-        assertHasPermissions(assessorAdmin_, adminToBeRemoved_);
-        assertHasPermissions(coordinator_, adminToBeRemoved_);
-        assertHasPermissions(navFeed_, adminToBeRemoved_);
-        assertHasPermissions(juniorMemberlist_, adminToBeRemoved_);
-        assertHasPermissions(seniorMemberlist, adminToBeRemoved_);
-        
-        // check if new admin has no permissions
-        assertHasNoPermissions(assessorAdmin_, newAdmin_);
-        assertHasNoPermissions(juniorMemberlist_, newAdmin_);
-        assertHasNoPermissions(seniorMemberlist, newAdmin_);
-        
-        // give spell permissions on root contract
-        AuthLike(root_).rely(spell_);
-
+    function testCastThenDeploy() public {
+        AuthLike(spell.SHELF()).rely(spell_);
         spell.cast();
-
-        // check if admin to be removed has no permissions anymore
-        assertHasNoPermissions(assessor_, adminToBeRemoved_);
-        assertHasNoPermissions(assessorAdmin_, adminToBeRemoved_);
-        assertHasNoPermissions(coordinator_, adminToBeRemoved_);
-        assertHasNoPermissions(navFeed_, adminToBeRemoved_);
-        assertHasNoPermissions(juniorMemberlist_, adminToBeRemoved_);
-        assertHasNoPermissions(seniorMemberlist, adminToBeRemoved_);
-
-        // check if new admin got permisions
-        assertHasPermissions(assessorAdmin_, newAdmin_);
-        assertHasPermissions(juniorMemberlist_, newAdmin_);
-        assertHasPermissions(seniorMemberlist, newAdmin_);
-
-        // check if minimum epoch time was set correctly
-        assertEq(coordinator.minimumEpochTime(), spell.minEpochTime());
-        
-        //check riskGroups
-        (,,uint ratePerSecond5,,) = pile.rates(5);
-        assertEq(ratePerSecond5, uint(1000000003805175038051750380));
-    }
-
-    function assertHasPermissions(address con, address ward) public {
-        uint perm = AuthLike(con).wards(ward);
-        assertEq(perm, 1);
-    }
-
-    function assertHasNoPermissions(address con, address ward) public {
-        uint perm = AuthLike(con).wards(ward);
-        assertEq(perm, 0);
+        RootLike(root_).deploy();
     }
 }
