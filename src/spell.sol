e6a137433800645fd4fac0f32273208c6672f441
Jeroen Offerijns <jeroen@offerijns.com>
2021-03-30 09:42:54 +0200

    fix: update description, remove unnecessary depend

--- src/spell.sol
+++ src/spell.sol
@@ -22,18 +22,17 @@ interface RootLike {
     function deploy() external;
 }
 
-// This spell fixes the permissions and risk groups of the PZ pool
+// This spell fixes the wiring of the shelf of the PZ1 pool
 contract TinlakeSpell {
 
     bool public done;
-    string constant public description = "Tinlake Mainnet Spell";
+    string constant public description = "Fix for the PZ1 shelf wiring";
 
     // PZ1 contracts
     address constant public ROOT = 0x92332a9831AC04275bC0f22b9140b21c72984EB8;
     address constant public SHELF = 0xBd9F2C1B2983e7923b3029c11BA485496F938564;
     address DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address wCUSD = 0xad3E3Fc59dff318BecEaAb7D00EB4F68b1EcF195;
-    address RESERVE = 0x7f5dEa6c463A7250c53F1347f82B506F40E1b0cB;
 
     function cast() public {
         require(!done, "spell-already-cast");
@@ -45,6 +44,5 @@ contract TinlakeSpell {
         ShelfLike(SHELF).depend("token", DAI);
         ShelfLike(SHELF).depend("lender", ROOT);
         ShelfLike(SHELF).depend("token", wCUSD);
-        ShelfLike(SHELF).depend("lender", RESERVE);
     }   
 }
