8095f2f342287fa6da8196f8eb68a474a178d5fc
Adam Stockermans <adamstox@protonmail.com>
2022-01-05 23:04:35 -0400

    Add HTC2, FF1, and AEA addresses

---
+++ src/draft/addresses_AEA.sol
@@ -0,0 +1,38 @@
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.7.0;
+
+contract Addresses {
+	address constant public ROOT_CONTRACT = 0xA1C16730637445D141d6ee35E31a67993FBe3759;
+	address constant public TINLAKE_CURRENCY = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+	address constant public TITLE = 0xd3eB623707a3e54B5ebAcaCf317EF596C90167d0;
+	address constant public PILE = 0x4b0f712Aa9F91359f48D8628De8483B04530751a;
+	address constant public SHELF = 0x3d4739821A0baF0a150ca4E49253455C341A4C97;
+	address constant public COLLECTOR = 0xbd47A652eaDfd50423115C81c0f4B74baE4F3aAE;
+	address constant public FEED = 0xfF74CE45e5190e9ce5e322E1C9Ac4FbBF57454bF;
+	address constant public JUNIOR_OPERATOR = 0xD40b2569Db1357475267e770f39E14E921618db3;
+	address constant public SENIOR_OPERATOR = 0x7693635DD007E7967B9e4AdF9A151f8eF738b1C4;
+	address constant public JUNIOR_TOKEN = 0x48154F5641D3E96504b6C6C7b3d3852D165f3Ff9;
+	address constant public SENIOR_TOKEN = 0x20217cbDE2A609E429D84f7a267cE229Dc1506F2;
+	address constant public JUNIOR_MEMBERLIST = 0x5B510F59eaD492251E93C487dCE31bDfaf6a415D;
+	address constant public SENIOR_MEMBERLIST = 0xf02bD1eDBbC076c95C9aB18747D8f0c2d0365d58;
+	address constant public COORDINATOR = 0xa3f5c408436c03f0fe58f32bb7FF17A4Cf18549E;
+	address constant public ASSESSOR  = 0x4fCD4E8453a582157f1085821aCAf3bd19Ef3FE2;
+	address constant public RESERVE = 0xDCb02156a7661c5Fd69252134761351a98845FF6;
+	address constant public SENIOR_TRANCHE = 0xb3ea49a5Da1e888a08Bf98Dd156773567a979477;
+	address constant public JUNIOR_TRANCHE = 0x700ae7edc6DA596A22f476e40fB59212Ec50CbC3;
+	address constant public POOL_ADMIN = 0x62A14b91A72efC818A14A24d9BE6689163f832E8;
+	address constant public CLERK = 0xC5B888722069bCDFf4cAb541Da6CD09B0658d079;
+	address constant public MGR = 0x8Fe38D1E4293181273E2e323e4c16e0D1d4861e3;
+
+    address constant public SPOTTER = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
+    address constant public VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address constant public JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+
+	address constant public ADMIN1 = address(0x71d9f8CFdcCEF71B59DD81AB387e523E2834F2b8);
+	address constant public ADMIN2 = address(0x46a71eEf8DbcFcbAC7A0e8D5d6B634A649e61fb8);
+    address constant public ADMIN3 = address(0x9eDec77dd2651Ce062ab17e941347018AD4eAEA9);
+	address constant public ADMIN4 = address(0xEf270f8877Aa1875fc13e78dcA31f3235210368f);
+	address constant public ADMIN5 = address(0xddEa1De10E93c15037E83b8Ab937A46cc76f7009);
+}
+
---
+++ src/draft/addresses_HTC2.sol
@@ -0,0 +1,38 @@
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.7.0;
+
+contract Addresses {
+	address constant public ROOT_CONTRACT = 0x4cA805cE8EcE2E63FfC1F9f8F2731D3F48DF89Df;
+	address constant public TINLAKE_CURRENCY = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+	address constant public TITLE = 0x669Db70d3A0D7941F468B0d907E9d90BD7ddA8d1;
+	address constant public PILE = 0xE7876f282bdF0f62e5fdb2C63b8b89c10538dF32;
+	address constant public SHELF = 0x5b2b43b3676057e38F332De73A9fCf0F8f6Babf7;
+	address constant public COLLECTOR = 0xDdA9c8631ea904Ef4c0444F2A252eC7B45B8e7e9;
+	address constant public FEED = 0xdB9A84e5214e03a4e5DD14cFB3782e0bcD7567a7;
+	address constant public JUNIOR_OPERATOR = 0x6DAecbC801EcA2873599bA3d980c237D9296cF57;
+	address constant public SENIOR_OPERATOR = 0xEDCD9e36017689c6Fc51C65c517f488E3Cb6C381;
+	address constant public JUNIOR_TOKEN = 0xAA67Bb563e14fBd4E92DCc646aAac0c00c7d9526;
+	address constant public SENIOR_TOKEN = 0xd511397f79b112638ee3B6902F7B53A0A23386C4;
+	address constant public JUNIOR_MEMBERLIST = 0x0b635CD35fC3AF8eA29f84155FA03dC9AD0Bab27;
+	address constant public SENIOR_MEMBERLIST = 0x1Bc55bcAf89f514CE5a8336bEC7429a99e804910;
+	address constant public COORDINATOR = 0x37f3D10Bd18124a16f4DcCA02D41F910E3Aa746A;
+	address constant public ASSESSOR  = 0xf2ED14102ee9D86606Ec24E48e89060ADB6DeFdb;
+	address constant public RESERVE = 0x86284A692430c25EfF37007c5707a530A6d63A41;
+	address constant public SENIOR_TRANCHE = 0x7E410F288583BfEe30a306F38e451a93Caaa5C47;
+	address constant public JUNIOR_TRANCHE = 0x7fe1dBcBEA4e6D3846f5caB67cfC9fce39BF4d71;
+	address constant public POOL_ADMIN = 0xad88b6F193bF31Be0a44A2914809BC517b03D22e;
+	address constant public CLERK = 0xcC2B64dC91245110B513f0ad1393a9720F66B996;
+	address constant public MGR = 0xe1ed3F588A98bF8a3744f4BF74Fd8540e81AdE3f;
+
+    address constant public SPOTTER = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
+    address constant public VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address constant public JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+
+	address constant public ADMIN1 = address(0x71d9f8CFdcCEF71B59DD81AB387e523E2834F2b8);
+	address constant public ADMIN2 = address(0x46a71eEf8DbcFcbAC7A0e8D5d6B634A649e61fb8);
+    address constant public ADMIN3 = address(0x9eDec77dd2651Ce062ab17e941347018AD4eAEA9);
+	address constant public ADMIN4 = address(0xEf270f8877Aa1875fc13e78dcA31f3235210368f);
+	address constant public ADMIN5 = address(0xddEa1De10E93c15037E83b8Ab937A46cc76f7009);
+}
+
